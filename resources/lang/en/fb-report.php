9a0b9ee050831d44ca793c5c3e584ea5e6a0957b
Morteza Masumi <morteza.masumi@gmail.com>
2025-08-18 05:21:12 +0330

    fix: minor

--- resources/lang/en/fb-report.php
+++ resources/lang/en/fb-report.php
@@ -7,8 +7,6 @@ return [
     'return' => 'Return',
     'label' => 'Report :label',
     'download' => 'Download report',
-    // 'page_full'   => 'Page :page of :total',
-    // 'report_date' => 'Date: :date',
     'heading' => 'Report :heading',
     'action' => 'Report',
     'columns' => 'Columns',
